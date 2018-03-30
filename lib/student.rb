ca50b44cb5cd2536c718b64b4c71ae7ebb426c7c
Neil Seejoor <nvseejoor@gmail.com>
2018-03-30 20:14:08 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -96,7 +96,7 @@ class Student
 
   def self.all_students_in_grade_X(x)
     sql = <<-SQL
-          SELECT COUNT(grade) FROM students WHERE grade = (?)
+          SELECT * FROM students WHERE grade = (?)
           SQL
     DB[:conn].execute(sql, x)
   end
