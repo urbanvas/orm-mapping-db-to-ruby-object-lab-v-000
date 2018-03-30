fb8c695fa0264eed674824901f5e6831e4a9e757
Neil Seejoor <nvseejoor@gmail.com>
2018-03-30 20:00:07 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -81,7 +81,7 @@ class Student
 
   def self.first_X_students_in_grade_10(x)
     sql = <<-SQL
-          SELECT * FROM students WHERE grade = 10 LIMIT (2)
+          SELECT * FROM students WHERE grade = 10 LIMIT (x)
           SQL
         # binding.pry
         DB[:conn].execute(sql, x)
